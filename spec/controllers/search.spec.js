37a4b0908816185db8db9d903625c60339db1309
Jose Diaz <jose.a.diaz@aguaseca.com>
2015-01-05 19:21:56 -0700

    Add unit testing using Jasmine and Karma.

---
+++ spec/controllers/search.spec.js
@@ -0,0 +1,8 @@
+describe('search controller', function() {
+    it('should load the search controller when the application is accessed');
+    describe('search', function() {
+        it('should have a method to allow for searching the web');
+        it('should return an array of results');
+        it('results should have a title, a description and a url to link to the site');
+    });
+});
