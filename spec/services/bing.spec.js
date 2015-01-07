6d6e0ec82efee7c0ff9a1cf9cc2a3be16d2b64d1
Jose Diaz <jose.a.diaz@aguaseca.com>
2015-01-06 22:17:12 -0700

    add service tests

---
+++ spec/services/bing.spec.js
@@ -0,0 +1,8 @@
+describe ('Bing Service', function() {
+
+  beforeEach(module('search.demo'));
+  
+  xit('should send a web query to the Bing API');
+  xit('should send a image query to the Bing API');
+  xit('should send a news query to the Bing API');
+});
