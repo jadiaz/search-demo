d25603918e728b9dfa0b7d3aba50a3588ced4d74
Jose Diaz <jose.a.diaz@aguaseca.com>
2015-01-07 17:59:13 -0700

    add more tests for bing service

--- spec/services/bing.spec.js
+++ spec/services/bing.spec.js
@@ -7,10 +7,32 @@ describe ('Bing Service', function() {
     bingSvc = $injector.get('BingService');
   }));
 
-  it('should allow a generic web search to be made', function () {
-    pending();
+  it('should set error if no search input is passed in', function () {
+    var searchStr = 'xbox';
+    var searchResults = []; 
+    bingSvc.webSearch(searchStr).then(
+      function (data) {
+        console.log(data);
+        searchResults = data;
+      },
+      function (error) {
+        console.log(error);
+      }
+    ).catch(function(err){
+      console.log(err);
+    });
+
+    expect(searchResults.length).not.toBe(0);
+  });
+
+  it('should return results based on search query passed in', function () {
+    var searchResults = [];
+    bingSvc.webSearch('xbox').then(function (data) {
+      searchResults = data;
+      expect(searchResults.length).toBe(0);
+    });
   });
 
   xit('should send a image query to the Bing API');
   xit('should send a news query to the Bing API');
-});
+}); 
